0d252eab42c1c29167f12de4941f5df719088ec7
swallowtail62 <swallowtail.a7@gmail.com>
2021-01-31 16:15:39 +0900

    Add dependency `askama` template engine, and use it

--- todo/src/error.rs
+++ todo/src/error.rs
@@ -2,6 +2,9 @@ use actix_web::ResponseError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum MyError {}
+pub enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error)
+}
 
 impl ResponseError for MyError {}
--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,13 +1,37 @@
 use actix_web::{App, get, HttpResponse, HttpServer};
+use askama::Template;
 
 use crate::error::MyError;
 
 mod error;
 
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
 #[get("/")]
 async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello world!";
-    Ok(HttpResponse::Ok().body(response_body))
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
