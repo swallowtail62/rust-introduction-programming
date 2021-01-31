455511e296586956281a6abfa3591f5604e8509f
swallowtail62 <swallowtail.a7@gmail.com>
2021-01-31 17:24:08 +0900

    Use database `sqlite` and add dependencies for database connection

--- todo/src/error.rs
+++ todo/src/error.rs
@@ -4,7 +4,13 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum MyError {
     #[error("Failed to render HTML")]
-    AskamaError(#[from] askama::Error)
+    AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error)
 }
 
 impl ResponseError for MyError {}
